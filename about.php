6610a46a776500d4baf4dcf7c973c1437c097d6b
Simon Meusel <simon.meusel.pc@t-online.de>
2016-03-10 20:33:07 +0100

    Add about page

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php include ("html/templateTop.php"); ?>
+
+<h1>BookStore</h1>
+
+<p>
+  Wenn du mehr Informationen über dieses Projekt haben willst,
+  oder du dir den Quellcode anschauen willst,
+  kannst du auf GitHub vorbeischauen:
+</p>
+<a href="https://github.com/simonmeusel/bookstore">BookStore auf GitHub</a>
+<?php include ("html/templateBottom.php"); ?>
