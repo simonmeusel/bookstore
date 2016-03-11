0d80f97c161323fc2e8a1a792724bd9c2e470139
simonmeusel <simon.meusel.pc@t-online.de>
2016-03-11 15:46:23 +0100

    Change database (tables: book, took)

--- database/BookStore.sql
+++ database/BookStore.sql
@@ -1,8 +1,20 @@
--- This is a sample database which you can insert
--- Created with phpMyAdmin
+-- phpMyAdmin SQL Dump
+-- version 4.5.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 11, 2016 at 03:42 PM
+-- Server version: 5.6.28-0ubuntu0.15.10.1
+-- PHP Version: 5.6.11-1ubuntu3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+01:00";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `BookStore`
@@ -16,11 +28,17 @@ SET time_zone = "+01:00";
 
 CREATE TABLE `book` (
   `id` int(11) NOT NULL,
+  `bid` varchar(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `isbn` varchar(20) NOT NULL,
   `author` varchar(50) NOT NULL,
   `message` varchar(100) NOT NULL,
   `date` date NOT NULL,
+  `publisher` varchar(50) NOT NULL,
+  `giver` int(11) NOT NULL,
+  `field` varchar(10) NOT NULL,
+  `publishingDate` date NOT NULL,
+  `price` varchar(10) NOT NULL,
   `taken` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='at';
 
@@ -35,6 +53,7 @@ CREATE TABLE `took` (
   `name` varchar(50) NOT NULL,
   `date` date NOT NULL,
   `deadline` date NOT NULL,
+  `class` varchar(5) NOT NULL,
   `notice` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -86,9 +105,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `took`
 --
 ALTER TABLE `took`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
